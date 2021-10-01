4ef3d805d790ce8b9eb162b682abbfbbeeb7fe26
Ryan Lewin <ryandevlewin@gmail.com>
2021-10-01 20:49:48 +1000

    Completes exercise 1.1

---
+++ part1/unicafe/src/components/Total.js
@@ -0,0 +1,10 @@
+
+const Total = props => {
+	return (
+		<section>
+			<p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+		</section>
+	)
+}
+
+export default Total
