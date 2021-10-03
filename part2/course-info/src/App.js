9cb7a036b855827a83cc3c9b58c26deeccd7ebe3
Ryan Lewin <ryandevlewin@gmail.com>
2021-10-03 13:05:07 +1000

    Completes exercise 2.1

--- part2/course-info/src/App.js
+++ part2/course-info/src/App.js
@@ -1,22 +1,40 @@
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Total from "./components/Total";
+import Course from "./components/Course";
 
 function App() {
+  // const course = {
+  //   name: 'Half Stack application development',
+  //   parts: [
+  //     {name: 'Fundamentals of React', exercises: 10},
+  //     {name: 'Using props to pass data', exercises: 7},
+  //     {name: 'State of a component', exercises: 14},
+  //   ]
+  // }
+
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
-      {name: 'Fundamentals of React', exercises: 10},
-      {name: 'Using props to pass data', exercises: 7},
-      {name: 'State of a component', exercises: 14},
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
     ]
   }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Course course={course} />
     </div>
   );
 }
